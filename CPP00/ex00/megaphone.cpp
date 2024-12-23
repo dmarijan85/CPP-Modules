4a59087cd071598c8435687c0d6894d26b14535e
Danilo Karol Marijanac <dmarijan@cbr15s6.42barcelona.com>
2024-12-23 13:55:37 +0100

    ex00 finished

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,18 +6,20 @@
 /*   By: dmarijan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 17:32:12 by dmarijan          #+#    #+#             */
-/*   Updated: 2024/12/21 17:36:54 by dmarijan         ###   ########.fr       */
+/*   Updated: 2024/12/23 13:54:41 by dmarijan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cstring>
+#include <cctype>
 
 int main(int argc, char **argv)
 {
 	size_t	i;
 	int		j = 1;
 
+	//custom message si no hay args
 	if (argc == 1)
 		std::cout << " * LOUD AND UNBEARABLE FEEDBACK NOISE * " << std::endl;
 	else
@@ -25,10 +27,25 @@ int main(int argc, char **argv)
 		while (argv[j])
 		{
 			for (i=0; i < strlen(argv[j]); i++)
-				std::cout << argv[j][i];
+			{
+				//si es letra y minuscula, printea una mayuscula en su lugar
+				if (std::isalpha(argv[j][i]) && std::islower(argv[j][i]))
+					std::cout << (char)(argv[j][i] - 32);
+				else
+				{
+					//si estamos en un espacio y estamos al principio o final del string o 
+					//hay mas espacios a seguir, no imprimimos.
+					if (argv[j][i] == ' ' && (i == 0 || i == strlen(argv[j])-1 || argv[j][i+1] == ' '))
+						;
+					else
+						std::cout << argv[j][i];
+				}
+			}
+			//imprimimos el UNICO espacio entre argumentos y chilleamos
 			std::cout << " ";
 			j++;
 		}
+		//una unica endline
 		std::cout << std::endl;
 	}
 	return (0);
