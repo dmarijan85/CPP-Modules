2f2790a17599d88e50b1847bc385be566b1047c2
Danilo Karol Marijanac <dmarijan@cbr14s6.42barcelona.com>
2025-01-03 12:36:28 +0100

    cpp00 finish

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: dmarijan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 17:32:12 by dmarijan          #+#    #+#             */
-/*   Updated: 2024/12/23 13:54:41 by dmarijan         ###   ########.fr       */
+/*   Updated: 2025/01/03 12:30:34 by dmarijan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,18 +28,12 @@ int main(int argc, char **argv)
 		{
 			for (i=0; i < strlen(argv[j]); i++)
 			{
-				//si es letra y minuscula, printea una mayuscula en su lugar
-				if (std::isalpha(argv[j][i]) && std::islower(argv[j][i]))
-					std::cout << (char)(argv[j][i] - 32);
+				//si estamos en un espacio y estamos al principio o final del string o 
+				//hay mas espacios a seguir, no imprimimos.
+				if (argv[j][i] == ' ' && (i == 0 || i == strlen(argv[j])-1 || argv[j][i+1] == ' '))
+					;
 				else
-				{
-					//si estamos en un espacio y estamos al principio o final del string o 
-					//hay mas espacios a seguir, no imprimimos.
-					if (argv[j][i] == ' ' && (i == 0 || i == strlen(argv[j])-1 || argv[j][i+1] == ' '))
-						;
-					else
-						std::cout << argv[j][i];
-				}
+					std::cout << (char)(toupper(argv[j][i]));
 			}
 			//imprimimos el UNICO espacio entre argumentos y chilleamos
 			std::cout << " ";
